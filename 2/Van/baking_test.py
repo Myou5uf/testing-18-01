1747c431cc50499f345f6ec113572047fbef9298
whyusoserious <kegont50@gmail.com>
2022-02-21 15:16:56 +0700

    Add tests and solutions (Щербаков, Петрунин, Шинкарук)

--- 2/Van/baking_test.py
+++ 2/Van/baking_test.py
@@ -4,9 +4,10 @@ import banking
 
 class dataTest(unittest.TestCase):
     def test_pastToFuture(self):
-        self.assertEqual(banking.change(40,[1,3,7,12,32]),'1 7 32')  # add assertion here
+        self.assertEqual(banking.change(40, [1, 3, 7, 12, 32]), '1 7 32')  # add assertion here
     def test_futureToFuture(self):
-        self.assertEqual(banking.change(99,[10, 50, 100, 500, 1000]),"impossible")
+        self.assertEqual(banking.change(99, [10, 50, 100, 500, 1000]), "impossible")
+
 
 if __name__ == '__main__':
     unittest.main()

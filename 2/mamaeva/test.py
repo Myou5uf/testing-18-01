e071a4b5efbc899ecbc6f192a35e188833416ac7
Myou5uf <myxamed12l@gmail.com>
2022-03-14 15:38:11 +0700

    TestCase for Mamaeva

--- 2/mamaeva/test.py
+++ 2/mamaeva/test.py
@@ -3,17 +3,29 @@ import task
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(task.lazy(2, ['1 2', '3 4']), 'NO')  # add assertion here\
+    def test_nameError(self):
+        self.assertRaises(ValueError, task.lazy, 2, ['1.5 2', '2 3'])
 
-    def test_positive(self):
-        self.assertEqual(task.lazy(3, ['1 2', '2 3', '2 4']), 'YES')
+    def test_typeErrorStr(self):
+        self.assertRaises(TypeError, task.lazy, '1', ['1 2', '2 3'])
 
-    def test_positive(self):
-        self.assertEqual(task.lazy(5, ['1 2', '2 3', '4 5', '1 5', '29 30']), 'NO')
+    def test_typeErrorInt(self):
+        self.assertRaises(TypeError, task.lazy, 2, 1, 2, 2, 3)
 
-    def test_positive(self):
-        self.assertEqual(task.lazy(4, ['5 7', '1 5', '3 5', '5 8']), 'YES')
+    def test_attributeError(self):
+        self.assertRaises(AttributeError, task.lazy, 1, [1, 2, 2, 3])
+
+    def test_minCountN(self):
+        self.assertRaises(IndexError, task.lazy, 0, ['1 2', '2 3'])
+
+    def test_maxCountN(self):
+        self.assertRaises(IndexError, task.lazy, 101, ['1 2', '2 3'])
+
+    def test_minCountA(self):
+        self.assertRaises(IndexError, task.lazy, 2, ['0 1', '1 2'])
+
+    def test_maxCountA(self):
+        self.assertRaises(IndexError, task.lazy, 2, ['1 29', '32 33'])
 
 
 if __name__ == '__main__':

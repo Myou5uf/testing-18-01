14ebd3b50fb986e6d744fa5cc2a797c0af2c9a7b
Aska <aska_2700@mail.ru>
2022-03-21 15:32:40 +0800

    Add tests(Шефтель)

--- 2/sheftel/test.py
+++ 2/sheftel/test.py
@@ -8,16 +8,19 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(task.solve(8, ['BAY', 'PET', 'RAY', 'BET', 'ONE', 'TWO', 'BAT', 'RAT']), (4, ['BAY', 'BAT', 'BET', 'PET']))
 
     def test_negative(self):
-        self.assertEqual(task.solve(5, ['AA', 'BB', 'AC', 'CB', 'ZZ']), (0, []))
+        self.assertEqual(task.solve(5, ['AA', 'BB', 'AC', 'CB', 'ZZ']), 0)
 
-    def test_empty_mismatch_1(self):
-        self.assertIsInstance(task.solve(0, ['11', 'aa']), str)
+    def test_type_mismatch_1(self):
+        self.assertRaises(TypeError, task.solve, ('Word', ['11', 'aa']))
 
-    def test_input_mismatch_2(self):
-        self.assertIsInstance(task.solve(12, []), str)
+    def test_type_mismatch_2(self):
+        self.assertRaises(TypeError, task.solve, 12, 8)
 
-    def test_input_negative(self):
-        self.assertIsInstance(task.solve(-5, []), str)
+    def test_index_mismatch_1(self):
+        self.assertRaises(IndexError, task.solve, -5, [])
+
+    def test_index_mismatch_2(self):
+        self.assertRaises(IndexError, task.solve, 3, [])
 
 
 if __name__ == '__main__':

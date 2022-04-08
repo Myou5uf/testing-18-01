6ceb05351e27019a396af844f0529d9387bb9add
egiptolog <idontsmoke00@mail.ru>
2022-04-08 20:14:29 +0700

    update tests(Ahmed-Zoda)

--- 2/ali/test.py
+++ 2/ali/test.py
@@ -9,22 +9,19 @@ class MyTestCase(unittest.TestCase):
                          (3, '12:12', '12:41'))
 
     def test_works_2(self):
-        self.assertEqual(task.solve(1, ['12:00', '18:00']), (1, '12:12', '12:41'))
+        self.assertEqual(task.solve(1, [['12:00', '18:00']]), (1, '12:00', '18:00'))
 
     def test_data_mismatch_1(self):
-        self.assertRaises(ValueError, task.solve(2, [['8:46', '12:41'], ['11:33', '14:45'], ['12:12', '15:56']]))
+        self.assertRaises(ValueError, task.solve, 2, [['8:46', '12:41'], ['11:33', '14:45'], ['12:12', '15:56']])
 
     def test_data_mismatch_2(self):
-        self.assertRaises(ValueError, task.solve(3, [['8:46'], ['11:33', '14:45'], ['12:12', '15:56']]))
+        self.assertRaises(ValueError, task.solve, 3, [['8:46'], ['11:33', '14:45'], ['12:12', '15:56']])
 
     def test_type_mismatch_1(self):
-        self.assertRaises(TypeError, task.solve('3', [['8:46', '12:41'], ['11:33', '14:45'], ['12:12', '15:56']]))
+        self.assertRaises(TypeError, task.solve, '3', [['8:46', '12:41'], ['11:33', '14:45'], ['12:12', '15:56']])
 
     def test_type_mismatch_2(self):
-        self.assertRaises(TypeError, task.solve(3, [[8, 41], ['11:33', 14], ['12:12', '15:56']]))
-
-    def test_type_mismatch_3(self):
-        self.assertRaises(TypeError, task.solve(3, ['212.65.71.73', '212.65.71.79', 212]))
+        self.assertRaises(TypeError, task.solve, 3, [[8, 41], ['11:33', 14], ['12:12', '15:56']])
 
     def test_borders_1(self):
         self.assertRaises(ValueError, task.solve, 0, [])

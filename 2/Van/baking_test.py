a50265d8656facd4ab926185ec342013a1c9d8b2
Admin <aziz89131983189@gmail.com>
2022-04-21 12:46:02 +0700

    add test in task sherbakov

--- 2/Van/baking_test.py
+++ 2/Van/baking_test.py
@@ -4,7 +4,7 @@ import banking
 
 class dataTest(unittest.TestCase):
     def test_pastToFuture(self):
-        self.assertEqual(banking.change(40, [1, 3, 7, 12, 32]), '1 7 32')  # add assertion here
+        self.assertEqual(banking.change(40, [1, 3, 7, 12, 32]).strip(), '1 7 32')  # add assertion here
     def test_futureToFuture(self):
         self.assertEqual(banking.change(99, [10, 50, 100, 500, 1000]), "impossible")
 
